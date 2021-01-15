22e1c16f00ea5b38e7fc3c06b407a86a3f97f8cc
LaBrew T2 <labrewt2@hawkmail.newpaltz.edu>
2021-01-15 15:35:28 -0500

    Added sql database

---
+++ .history/Database/seed_20210115153355.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("veggie burger", false);
---
+++ .history/Database/seed_20210115153412.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", false);
+
+INSERT INTO burgers (burger_name, eaten)
+VALUES ("bacon cheeseburger", false);
+
+INSERT INTO burgers (burger_name, eaten)
+VALUES ("veggie burger", false);
